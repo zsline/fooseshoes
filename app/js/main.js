efb708f44f2cd98e9003767c4804406d8188803c
zsline <sergei.dereka@gmal.com>
2021-09-10 13:56:05 +0300

    adeed sliders + dots

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function () {
+    $('.top__slider').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        asNavFor: '.active__slider'
+    });
+    $('.active__slider').slick({
+        slidesToShow: 5,
+        asNavFor: '.top__slider',
+        dots: true,
+        focusOnSelect: true
+    });
 
 })
